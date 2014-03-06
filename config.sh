6dee6bdc1add1dd8e0cbf00cb8e240acd2810825
Filippo Valsorda <filippo.valsorda@gmail.com>
2014-03-06 01:45:23 +0100

    M22: config/hubs/<hub-id> instead of config/<hub-id>

--- config.sh
+++ config.sh
@@ -4,7 +4,7 @@
 CONFIGDIR=~/smart-home-config/config
 
 UUID=$(cat ~/.hub-id)
-HUB_CONFIG="$CONFIGDIR/$UUID/config.$UUID.json"
+HUB_CONFIG="$CONFIGDIR/hubs/$UUID/config.$UUID.json"
 if [ -f "$HUB_CONFIG" ]
 then
     jq -s '.[0] + .[1]' "$CONFIGDIR/config.json" "$HUB_CONFIG"
