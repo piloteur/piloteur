648a5531c6bec69a404ec0d7663e9f5a8de33613
Filippo Valsorda <filippo.valsorda@gmail.com>
2014-03-07 04:55:32 +0100

    add some debug capabilities to the test driver

--- drivers/test/__main__.py
+++ drivers/test/__main__.py
@@ -1,5 +1,10 @@
 import time
+import sys
+import json
+
+config = json.loads(sys.stdin.read())
+print config
 
 while True:
-  print 'Hello world!'
+  print >> sys.stderr, 'Hello world!'
   time.sleep(10)
