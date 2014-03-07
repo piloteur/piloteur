6f53726a091f5d77c79b44773d7558e51c66b407
Filippo Valsorda <filippo.valsorda@gmail.com>
2014-03-07 16:08:21 +0100

    use the virtualenv to run drivers

--- smarthome-hub-watchdog/driver-wrapper.sh
+++ smarthome-hub-watchdog/driver-wrapper.sh
@@ -5,4 +5,6 @@ DRIVER=$2
 
 REDIRECTOR=smarthome-hub-watchdog/output-redirector.py
 
-{ ./config.sh | python -u "$DRIVER" 2>&3 | ~/ENV/bin/python "$REDIRECTOR" -d $NAME; } 3>&1 1>&2 | ~/ENV/bin/python "$REDIRECTOR" -l $NAME
+PYTHON=~/drivers_ENVs/$NAME/bin/python
+
+{ ./config.sh | $PYTHON -u "$DRIVER" 2>&3 | ~/ENV/bin/python "$REDIRECTOR" -d $NAME; } 3>&1 1>&2 | ~/ENV/bin/python "$REDIRECTOR" -l $NAME
