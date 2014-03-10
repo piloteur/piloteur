f111615176ee6db100f9db790f088b07fe53f1df
Filippo Valsorda <filippo.valsorda@gmail.com>
2014-03-10 15:51:05 +0100

    BLE: kill lescan on SIGTERM and monitor adapter health

--- drivers/ble_tag_monitor.py
+++ drivers/ble_tag_monitor.py
@@ -2,18 +2,30 @@ import subprocess
 import sys
 import json
 import time
+import threading
+import signal
 
 class BLE_driver():
     def __init__(self):
         self.lescan_pid = None
+        self.KILLED_BY_US = False
+        self.ALIVE = True
+
+        lsusb = subprocess.check_output("lsusb").split('\n')
+        blue = next(line for line in lsusb if 'bluetooth' in line.lower()) # XXX
+        self.bus, self.device = blue[4:7], blue[15:18]
 
     def run(self):
+        t = threading.Timer(15, self.monitor); t.daemon = True; t.start()
         while True:
-            self.reset()
+            if not self.KILLED_BY_US: self.reset()
+            time.sleep(2)
             self.start_lescan()
+            if not self.ALIVE: break
             print >> sys.stderr, 'ERROR: lescan quit, restarting'
 
     def start_lescan(self):
+        self.KILLED_BY_US = False
         p = subprocess.Popen(['sudo', 'lescan'], stdout=subprocess.PIPE)
         print >> sys.stderr, 'started lescan, pid %s' % p.pid
         self.lescan_pid = p.pid
@@ -23,17 +35,29 @@ class BLE_driver():
             data = json.loads(line)
             print '%(timestamp)s,%(bdaddr)s,%(rssi)s' % data
 
+    def monitor(self):
+        t = threading.Timer(15, self.monitor); t.daemon = True; t.start()
+        code = subprocess.call(["sudo", "hciconfig", "hci0", "lestates"])
+        if code != 0:
+            self.KILLED_BY_US = True
+            print >> sys.stderr, 'adapter dead, resetting'
+            self.reset()
+
     def reset(self):
-        lsusb = subprocess.check_output("lsusb").split('\n')
-        blue = next(line for line in lsusb if 'bluetooth' in line.lower()) # XXX
-        bus, device = blue[4:7], blue[15:18]
-        print >> sys.stderr, 'resetting usb %s %s' % (bus, device)
-        subprocess.check_call(["sudo", "usbreset", "/dev/bus/usb/%s/%s" % (bus, device)],
+        print >> sys.stderr, 'resetting usb %s %s' % (self.bus, self.device)
+        subprocess.check_call(["sudo", "usbreset", "/dev/bus/usb/%s/%s" % (self.bus, self.device)],
             stdout=sys.stderr)
-        time.sleep(2)
+
+    def exit(self, signum=None, frame=None):
+        print >> sys.stderr, 'exiting...'
+        self.ALIVE = False
+        self.reset()
+        sys.exit(0)
 
 def main():
     b = BLE_driver()
+    for sig in [signal.SIGTERM, signal.SIGINT, signal.SIGHUP, signal.SIGQUIT]:
+        signal.signal(sig, b.exit)
     b.run()
 
 if __name__ == '__main__':
