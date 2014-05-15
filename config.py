be689c98fc5ac20b2d7d7ee188178d663109ef44
Filippo Valsorda <filippo.valsorda@gmail.com>
2014-05-15 02:46:52 -0400

    M43: support config paths flexibility

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ import json
 import copy
 import sys
 import re
+import os
 
 def dmerge(d1, d2):
     if not isinstance(d2, dict):
@@ -17,8 +18,23 @@ def dmerge(d1, d2):
             result[k] = copy.deepcopy(v)
     return result
 
+def gen_paths(CONFIG_DIR):
+    paths = {}
+    for root, dirs, files in os.walk(CONFIG_DIR):
+        foldername = os.path.basename(root)
+        if not "config.%s.json" % foldername in files:
+            continue
+        if foldername in paths:
+            paths[foldername] = None
+            # TODO: log an error here
+        else:
+            paths[foldername] = os.path.join(root, "config.%s.json" % foldername)
+    return paths
+
 # TODO: un-hardcode this?
 CONFIG_DIR = os.path.expanduser('~/smart-home-config/config')
+
+config_paths = gen_paths(CONFIG_DIR)
 
 with open(os.path.join(CONFIG_DIR, "config.json")) as f:
     config = json.load(f)
@@ -29,10 +45,10 @@ with open(os.path.expanduser('~/.hub-classes')) as f:
     classes = re.split(r'[^a-z0-9-]+', f.read().strip())
 
 for name in classes + [UUID]:
-    new_config_path = os.path.join(CONFIG_DIR, "hubs", name, "config.%s.json" % name)
-    if os.path.isfile(new_config_path):
-        with open(new_config_path) as f:
-            new_config = json.load(f)
-        config = dmerge(config, new_config)
+    if not config_paths.get(name): continue
+    new_config_path = config_paths[name]
+    with open(new_config_path) as f:
+        new_config = json.load(f)
+    config = dmerge(config, new_config)
 
 json.dump(config, sys.stdout, indent=4)
