e1d2e86fb276401da9f57cf9b6758e9fcb849d0c
Filippo Valsorda <filippo.valsorda@gmail.com>
2014-06-19 17:03:23 +0200

    Remove broken log period handling

--- smarthome-hub-sync.py
+++ smarthome-hub-sync.py
@@ -12,7 +12,6 @@ import traceback
 import email.utils
 import urllib2
 import itertools
-import collections
 
 import socket
 socket.setdefaulttimeout(60)
@@ -58,8 +57,6 @@ class Syncer():
         self.LOG_HOUR = datetime.datetime.utcnow().strftime('%Y-%m-%d-%H')
 
         self.KEYFILE_PATH = os.path.expanduser(self.config['keyfile_path'])
-
-        self.last_runs = collections.defaultdict(int)
 
     # def run_remote_command(self, command):
     #     ssh_cmd = ["ssh", "-i", self.KEYFILE_PATH]
@@ -183,9 +180,6 @@ class Syncer():
                 (remote - local).total_seconds()))
 
     def versions(self):
-        if time.time() - self.last_runs["versions"] < 10 * 60: return
-        self.last_runs["versions"] = time.time()
-
         now = datetime.datetime.utcnow()
 
         VERSIONS_PATH = os.path.join(self.LOGS_PATH, "versions/versions-log.%s.csv" % self.LOG_HOUR)
@@ -207,9 +201,6 @@ class Syncer():
             f.write('%s,%s,%s\n' %(now.isoformat(), ansible, ','.join(versions)))
 
     def classes(self):
-        if time.time() - self.last_runs["classes"] < 10 * 60: return
-        self.last_runs["classes"] = time.time()
-
         CLASSES_PATH = os.path.join(self.LOGS_PATH, "classes/classes-log.%s.csv" % self.LOG_HOUR)
 
         with open(CLASSES_PATH, 'a') as f:
