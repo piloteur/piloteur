f752d3a10f9674e575eeb86a25b081c016fb3809
Filippo Valsorda <filippo.valsorda@gmail.com>
2014-07-22 12:42:27 -0700

    Move monitor logic into nexus

--- monitor_node/monitor.py
+++ monitor_node/monitor.py
@@ -12,13 +12,9 @@ Options:
 
 """
 
-import subprocess
 import os.path
 import json
 import re
-import arrow
-import datetime
-import collections
 import paramiko
 import time
 import fnmatch
@@ -29,32 +25,7 @@ from flask import render_template, abort, url_for
 import nexus
 import nexus.private
 
-YELLOW_LIMIT = datetime.timedelta(minutes=15)
-RED_LIMIT = datetime.timedelta(minutes=30)
-
-def void_namedtuple(ntuple):
-    void = ntuple._make([None] * len(ntuple._fields))
-    def new(**kwargs):
-        return void._replace(**kwargs)
-    return new
-
-NodeData = void_namedtuple(collections.namedtuple('NodeData',
-    ['hub_id', 'classes', 'timestamp', 'versions', 'wifi_quality', 'error', 'last_writes', 'config']))
-NodeResult = void_namedtuple(collections.namedtuple('NodeResult',
-    ['hub_id', 'classes', 'timestamp', 'versions', 'wifi_quality', 'error', 'summary', 'hub_health', 'drivers']))
-
-
-def get_tunnel_connections(tunnel_info):
-    username, hostname, port, folder = tunnel_info
-
-    client = paramiko.SSHClient()
-    client.load_system_host_keys()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(hostname, port, username)
-
-    cmd = "grep -h . {}*".format(folder)
-    stdin, stdout, stderr = client.exec_command(cmd)
-    return [n.strip() for n in stdout.readlines()]
+from nexus.monitor import get_tunnel_connections, fetch_data, assess_data
 
 
 class Monitor():
@@ -77,139 +48,6 @@ class Monitor():
 
         abort(500)
 
-    def fetch_data(self, hub_id):
-        nexus.private.set_hub_id(hub_id)
-
-        if hub_id not in nexus.list_hub_ids():
-            return NodeData(hub_id=hub_id, error="Hub ID not found.")
-
-        classes_log = nexus.private.fetch_system_logs("classes")
-        if not classes_log:
-            return NodeData(hub_id=hub_id, error="Missing classes data.")
-        remote_hub_id = classes_log.split(',')[0]
-        if not remote_hub_id == hub_id:
-            return NodeData(hub_id=hub_id, error="Mismatching hub_id?!")
-        classes = classes_log.split(',')[1:]
-
-        config_cmd = [os.path.expanduser("~/smarthome-hub-sync/config.py")]
-        config_cmd.append(hub_id)
-        config_cmd.extend(classes)
-        node_config = json.loads(subprocess.check_output(config_cmd))
-
-        timesync_log = nexus.private.fetch_system_logs("timesync")
-        if not timesync_log:
-            return NodeData(hub_id=hub_id, error="Missing timesync data.")
-        timestamp = arrow.get(timesync_log.split(',')[0])
-
-        iwconfig_log = nexus.private.fetch_system_logs("iwconfig")
-        if not iwconfig_log:
-            return NodeData(hub_id=hub_id, error="Missing iwconfig data.")
-        wifi_quality = iwconfig_log.split(',')[1]
-        if wifi_quality == 'N/A': wifi_quality = None
-        else: wifi_quality = int(wifi_quality)
-
-        versions_log = nexus.private.fetch_system_logs("versions")
-        if not versions_log:
-            return NodeData(hub_id=hub_id, error="Missing versions data.")
-        versions = versions_log.split(',')
-        versions = dict(zip((
-            "timestamp",
-            "ansible",
-            "smart-home-config",
-            "smarthome-deployment-blobs",
-            "smarthome-drivers",
-            "smarthome-hub-sync",
-            "smarthome-reverse-tunneler",
-        ), versions))
-
-        last_writes = {}
-        for driver_name in node_config['loaded_drivers']:
-            t = nexus.data_timestamp(driver_name)
-            if not t:
-                last_writes[driver_name] = arrow.get(0)
-            else:
-                last_writes[driver_name] = arrow.get(t)
-
-        return NodeData(hub_id=hub_id,
-                        classes=classes,
-                        config=node_config,
-                        timestamp=timestamp,
-                        versions=versions,
-                        last_writes=last_writes,
-                        wifi_quality=wifi_quality)
-
-    def assess_data(self, data):
-        if data.error:
-            return NodeResult(
-                hub_id=data.hub_id,
-                hub_health=nexus.RED,
-                error=data.error,
-            )
-
-        drivers = []
-        versions = []
-
-        hub_health = nexus.GREEN
-        error_message = ''
-
-
-        ########################################################################
-        # DRIVERS                                                              #
-        ########################################################################
-        for driver_name, last_write in sorted(data.last_writes.items()):
-            health = nexus.GREEN
-            if (data.timestamp - last_write) > YELLOW_LIMIT:
-                health = nexus.YELLOW
-            if (data.timestamp - last_write) > RED_LIMIT:
-                health = nexus.RED
-
-            if health != nexus.GREEN:
-                error_message += '"{}" last logged data {}. '.format(
-                    driver_name, last_write.humanize(data.timestamp))
-
-            hub_health = max(hub_health, health)
-            drivers.append((driver_name,
-                            last_write.humanize(data.timestamp),
-                            last_write.format('YYYY-MM-DD HH:mm:ss ZZ'),
-                            health))
-
-        ########################################################################
-        # VERSIONS                                                             #
-        ########################################################################
-        health = nexus.GREEN
-        ansible = data.versions['ansible']
-        if ansible != 'ansible 1.5.3':  # TODO unhardcode?
-            health = nexus.RED
-            error_message += 'Old Ansible version. '
-        hub_health = max(hub_health, health)
-        versions.append(('ansible', ansible, 'ansible 1.5.3', health))
-
-        del data.versions['timestamp']
-        del data.versions['ansible']
-
-        for repo, commit in data.versions.items():
-            # TODO get repo last commit and check how old is this
-            versions.append((repo, commit[:7], '', nexus.GREEN))
-
-        ########################################################################
-        # WI-FI QUALITY                                                        #
-        ########################################################################
-        if data.wifi_quality and data.wifi_quality < 30:
-            hub_health = max(hub_health, nexus.YELLOW)
-            error_message += 'Weak Wi-Fi signal. '
-
-
-        return NodeResult(
-            drivers=drivers,
-            timestamp=data.timestamp.format('YYYY-MM-DD HH:mm:ss ZZ'),
-            hub_id=data.hub_id,
-            hub_health=hub_health,
-            versions=versions,
-            classes=data.classes,
-            wifi_quality=data.wifi_quality,
-            summary=error_message,
-        )
-
 
     ### STATUS
 
@@ -222,8 +60,8 @@ class Monitor():
 
         self.nexus_init()
 
-        data = self.fetch_data(hub_id)
-        result = self.assess_data(data)
+        data = fetch_data(hub_id)
+        result = assess_data(data)
 
         return render_template('ajax_status.html', h=result, nexus=nexus)
 
@@ -252,8 +90,8 @@ class Monitor():
 
         results = []
         for hub_id in hubs:
-            data = self.fetch_data(hub_id)
-            res = self.assess_data(data)
+            data = fetch_data(hub_id)
+            res = assess_data(data)
 
             results.append((
                 res.hub_id,
@@ -290,8 +128,8 @@ class Monitor():
         for hub_id in hubs_list:
             app.logger.info(hub_id)
 
-            data = self.fetch_data(hub_id)
-            res = self.assess_data(data)
+            data = fetch_data(hub_id)
+            res = assess_data(data)
 
             if res.error:
                 color = nexus.RED
---
+++ nexus/monitor.py
@@ -0,0 +1,172 @@
+from __future__ import absolute_import, print_function, division, unicode_literals
+
+import subprocess
+import os.path
+import json
+import arrow
+import datetime
+import collections
+import paramiko
+
+from . import RED, YELLOW, GREEN, list_hub_ids, data_timestamp
+from . import private
+
+YELLOW_LIMIT = datetime.timedelta(minutes=15)
+RED_LIMIT = datetime.timedelta(minutes=30)
+
+def void_namedtuple(ntuple):
+    void = ntuple._make([None] * len(ntuple._fields))
+    def new(**kwargs):
+        return void._replace(**kwargs)
+    return new
+
+NodeData = void_namedtuple(collections.namedtuple('NodeData',
+    ['hub_id', 'classes', 'timestamp', 'versions', 'wifi_quality', 'error', 'last_writes', 'config']))
+NodeResult = void_namedtuple(collections.namedtuple('NodeResult',
+    ['hub_id', 'classes', 'timestamp', 'versions', 'wifi_quality', 'error', 'summary', 'hub_health', 'drivers']))
+
+
+def get_tunnel_connections(tunnel_info):
+    username, hostname, port, folder = tunnel_info
+
+    client = paramiko.SSHClient()
+    client.load_system_host_keys()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(hostname, port, username)
+
+    cmd = "grep -h . {}*".format(folder)
+    stdin, stdout, stderr = client.exec_command(cmd)
+    return [n.strip() for n in stdout.readlines()]
+
+def fetch_data(hub_id):
+    private.set_hub_id(hub_id)
+
+    if hub_id not in list_hub_ids():
+        return NodeData(hub_id=hub_id, error="Hub ID not found.")
+
+    classes_log = private.fetch_system_logs("classes")
+    if not classes_log:
+        return NodeData(hub_id=hub_id, error="Missing classes data.")
+    remote_hub_id = classes_log.split(',')[0]
+    if not remote_hub_id == hub_id:
+        return NodeData(hub_id=hub_id, error="Mismatching hub_id?!")
+    classes = classes_log.split(',')[1:]
+
+    config_cmd = [os.path.expanduser("~/smarthome-hub-sync/config.py")]
+    config_cmd.append(hub_id)
+    config_cmd.extend(classes)
+    node_config = json.loads(subprocess.check_output(config_cmd))
+
+    timesync_log = private.fetch_system_logs("timesync")
+    if not timesync_log:
+        return NodeData(hub_id=hub_id, error="Missing timesync data.")
+    timestamp = arrow.get(timesync_log.split(',')[0])
+
+    iwconfig_log = private.fetch_system_logs("iwconfig")
+    if not iwconfig_log:
+        return NodeData(hub_id=hub_id, error="Missing iwconfig data.")
+    wifi_quality = iwconfig_log.split(',')[1]
+    if wifi_quality == 'N/A': wifi_quality = None
+    else: wifi_quality = int(wifi_quality)
+
+    versions_log = private.fetch_system_logs("versions")
+    if not versions_log:
+        return NodeData(hub_id=hub_id, error="Missing versions data.")
+    versions = versions_log.split(',')
+    versions = dict(zip((
+        "timestamp",
+        "ansible",
+        "smart-home-config",
+        "smarthome-deployment-blobs",
+        "smarthome-drivers",
+        "smarthome-hub-sync",
+        "smarthome-reverse-tunneler",
+    ), versions))
+
+    last_writes = {}
+    for driver_name in node_config['loaded_drivers']:
+        t = data_timestamp(driver_name)
+        if not t:
+            last_writes[driver_name] = arrow.get(0)
+        else:
+            last_writes[driver_name] = arrow.get(t)
+
+    return NodeData(hub_id=hub_id,
+                    classes=classes,
+                    config=node_config,
+                    timestamp=timestamp,
+                    versions=versions,
+                    last_writes=last_writes,
+                    wifi_quality=wifi_quality)
+
+def assess_data(data):
+    if data.error:
+        return NodeResult(
+            hub_id=data.hub_id,
+            hub_health=RED,
+            error=data.error,
+        )
+
+    drivers = []
+    versions = []
+
+    hub_health = GREEN
+    error_message = ''
+
+
+    ########################################################################
+    # DRIVERS                                                              #
+    ########################################################################
+    for driver_name, last_write in sorted(data.last_writes.items()):
+        health = GREEN
+        if (data.timestamp - last_write) > YELLOW_LIMIT:
+            health = YELLOW
+        if (data.timestamp - last_write) > RED_LIMIT:
+            health = RED
+
+        if health != GREEN:
+            error_message += '"{}" last logged data {}. '.format(
+                driver_name, last_write.humanize(data.timestamp))
+
+        hub_health = max(hub_health, health)
+        drivers.append((driver_name,
+                        last_write.humanize(data.timestamp),
+                        last_write.format('YYYY-MM-DD HH:mm:ss ZZ'),
+                        health))
+
+    ########################################################################
+    # VERSIONS                                                             #
+    ########################################################################
+    health = GREEN
+    ansible = data.versions['ansible']
+    if ansible != 'ansible 1.5.3':  # TODO unhardcode?
+        health = RED
+        error_message += 'Old Ansible version. '
+    hub_health = max(hub_health, health)
+    versions.append(('ansible', ansible, 'ansible 1.5.3', health))
+
+    del data.versions['timestamp']
+    del data.versions['ansible']
+
+    for repo, commit in data.versions.items():
+        # TODO get repo last commit and check how old is this
+        versions.append((repo, commit[:7], '', GREEN))
+
+    ########################################################################
+    # WI-FI QUALITY                                                        #
+    ########################################################################
+    if data.wifi_quality and data.wifi_quality < 30:
+        hub_health = max(hub_health, YELLOW)
+        error_message += 'Weak Wi-Fi signal. '
+
+
+    return NodeResult(
+        drivers=drivers,
+        timestamp=data.timestamp.format('YYYY-MM-DD HH:mm:ss ZZ'),
+        hub_id=data.hub_id,
+        hub_health=hub_health,
+        versions=versions,
+        classes=data.classes,
+        wifi_quality=data.wifi_quality,
+        summary=error_message,
+    )
