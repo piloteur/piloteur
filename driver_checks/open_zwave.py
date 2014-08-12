77f47115e395e3820b0d38a61e8ed8e1f426e1bf
Filippo Valsorda <filippo.valsorda@gmail.com>
2014-08-13 01:54:02 +0200

    DRY up the code and add a open_zwave check

--- driver_checks/open_zwave.py
+++ driver_checks/open_zwave.py
@@ -3,8 +3,8 @@ import datetime
 from nexus import command_line
 from .common import data_freshness_check
 
-DRIVER_NAME = "nest_thermostat"
-RED_LIMIT = datetime.timedelta(minutes=1)
+DRIVER_NAME = "open_zwave"
+RED_LIMIT = datetime.timedelta(hours=24)
 
 check = data_freshness_check(DRIVER_NAME, RED_LIMIT)
 
