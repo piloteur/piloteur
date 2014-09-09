9cb60aa09c4c37316771cc56e7653fc8e6e9c305
Filippo Valsorda <filippo.valsorda@gmail.com>
2014-09-09 03:30:45 -0700

    Update the repo_overrides system and the pull one, now manual

--- deployment/roles/endpoint-pull/templates/ansible-pull.sh
+++ deployment/roles/endpoint-pull/templates/ansible-pull.sh
@@ -4,15 +4,50 @@
 . $HOME/.profile
 cd "{{ home }}"
 
-if ansible-pull -C "{{ deploy_rev }}" \
-                -i "{{ home }}/inventory.ini" \
-                -d "{{ deploy }}" \
-                -U "{{ deploy_repo }}" \
-                >> {{ logfile }} 2>&1;
-then
-    rm "{{ logfile }}"
-else
-    SYNCED_LOGFILE="{{ synced_logfile }}"
-    sudo mv "{{ logfile }}" "$SYNCED_LOGFILE"
-    sudo chown smarthome:smarthome "$SYNCED_LOGFILE"
-fi
+### STRICT MODE
+set -euo pipefail
+IFS=$'\n\t'
+
+readonly CODE="{{ home }}/piloteur-code"
+readonly INVENTORY="{{ home }}/inventory.ini"
+readonly LOGFILE="{{ logfile }}"
+readonly SYNCED_LOGFILE="{{ synced_logfile }}"
+readonly REPO="{{ code_repo }}"
+readonly REVISION="{{ code_rev }}"
+
+git_init() {
+    ls .git || git init .
+}
+
+git_checkout() {
+    (git remote -v | grep auto-piloteur) || git remote add auto-piloteur "$REPO"
+    git remote set-url auto-piloteur "$REPO"
+
+    git fetch --all
+    git reset --hard "auto-piloteur/$REVISION" || git reset --hard "$REVISION"
+}
+
+ansible_playbook() {
+    cd "$CODE/deployment"
+
+    ansible-playbook -i "$INVENTORY" endpoint_node_local.yml
+}
+
+teardown() {
+    sudo mv "$LOGFILE" "$SYNCED_LOGFILE"
+    sudo chown piloteur:piloteur "$SYNCED_LOGFILE"
+
+    exit 1
+}
+
+main() {
+    cd "$CODE"
+
+    git_init >> "$LOGFILE" 2>&1 || teardown
+    git_checkout >> "$LOGFILE" 2>&1 || teardown
+    ansible_playbook >> "$LOGFILE" 2>&1 || teardown
+
+    rm "$LOGFILE"
+}
+
+main
