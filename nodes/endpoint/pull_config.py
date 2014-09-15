12fbf1c80121c82d8f01bfb6860fa61d79bc92c0
Filippo Valsorda <filippo.valsorda@gmail.com>
2014-09-15 18:49:01 +0200

    CONFIG NODE/4: write and run the endpoint config puller

---
+++ nodes/endpoint/pull_config.py
@@ -0,0 +1,11 @@
+import os.path
+import urllib
+import sys
+
+ADDR = sys.argv[1]
+PATH = os.path.expanduser('~/config.json')
+
+with open(os.path.expanduser('~/.config-token')) as f:
+    TOKEN = f.read().strip()
+
+urllib.urlretrieve("%s/v1/%s/config.json", PATH)
